4f23bafc015a409f5e2cd555a46f0a86522e4991
oluwaniyii82 <65834266+oluwaniyii82@users.noreply.github.com>
2021-06-21 15:57:47 -0500

    Update 3-install-halyard.sh

--- spinnaker-data/scripts/3-install-halyard.sh
+++ spinnaker-data/scripts/3-install-halyard.sh
@@ -12,14 +12,14 @@ sudo bash InstallHalyard.sh --user ubuntu
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker ubuntu
-sudo docker run -p 127.0.0.1:9090:9000 -d --name minio1 -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data
+sudo docker run -p 18.224.170.172:9090:9000 -d --name minio1 -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data
 
 sudo apt-get -y install jq apt-transport-https
 
 MINIO_SECRET_KEY="minioadmin"
 MINIO_ACCESS_KEY="minioadmin"
 
-echo $MINIO_SECRET_KEY | hal config storage s3 edit --endpoint http://127.0.0.1:9090 \
+echo $MINIO_SECRET_KEY | hal config storage s3 edit --endpoint http://18.224.170.172:9090 \
     --access-key-id $MINIO_ACCESS_KEY \
     --secret-access-key
 
